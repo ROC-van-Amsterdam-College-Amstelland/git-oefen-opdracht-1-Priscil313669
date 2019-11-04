910d90c9b9a0dcec37011063b2b6dc0b67af4c19
PriscillaLumor <priscilla.lumor@student.rocva.nl>
2019-11-04 17:46:31 +0100

    js

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-function changecolor1 ()
-{
-    if(document.getElementById("menuItem_1").style.backgroundColor = "orange")
-    {
-        document.getElementById("menuItem_1").style.backgroundColor = "black"  
-    }
-}
